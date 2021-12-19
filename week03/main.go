21f265dbb42c405be6f28c754af2b96e979ad360
EvilMp <1273230678@qq.com>
2021-12-19 23:15:14 +0800

    并发类

---
+++ week03/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"log"
+	"errors"
+	"context"
+	"net/http"
+	"golang.org/x/sync/errgroup"
+)
+
+func main() {
+	g, ctx 			:= errgroup.WithContext(context.Background())
+
+	mux 			:= http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong"))
+	})
+
+	// 模拟单个服务错误退出
+	serverOut 		:= make(chan struct{})
+	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
+		serverOut <- struct{}{}
+	})
+
+	server 			:= http.Server{
+		Handler : 	mux,
+		Addr :    	":8080",
+	}
+
+	g.Go(func() error {
+		return server.ListenAndServe()
+	})
+
+	g.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("errgroup exit...")
+		case <-serverOut:
+			log.Println("server will out...")
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+
+		log.Println("shutting down server...")
+		return server.Shutdown(timeoutCtx)
+	})
+
+	// 捕获到 os 退出信号将会退出
+	g.Go(func() error {
+		quit := make(chan os.Signal, 0)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case sig := <-quit:
+			return errors.Errorf("get os signal: %v", sig)
+		}
+	})
+
+	log.Printf("errgroup exiting: %+v\n", g.Wait())
+}
+
