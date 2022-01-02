75e787e5c280220cc2f0cbd8718daa659d7a4052
EvilMp <1273230678@qq.com>
2022-01-02 23:18:55 +0800

    滑动个窗口

--- week03/main.go
+++ week03/main.go
@@ -14,15 +14,17 @@ import (
 )
 
 func main() {
-	g, ctx 			:= errgroup.WithContext(context.Background())
+	eg, ctx 		:= errgroup.WithContext(context.Background())
 
 	mux 			:= http.NewServeMux()
+	serverOut 		:= make(chan struct{})
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("server test"))
+	})
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("pong"))
 	})
-
-	// 模拟单个服务错误退出
-	serverOut 		:= make(chan struct{})
 	mux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
 		serverOut <- struct{}{}
 	})
@@ -32,38 +34,41 @@ func main() {
 		Addr :    	":8080",
 	}
 
-	g.Go(func() error {
+	// 开始监听
+	eg.Go(func() (err error) {
+		log.Printf("[info] start http server listening %s \n", server.Addr)
 		return server.ListenAndServe()
 	})
 
-	g.Go(func() error {
-		select {
-		case <-ctx.Done():
-			log.Println("errgroup exit...")
-		case <-serverOut:
-			log.Println("server will out...")
-		}
-
-		timeoutCtx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-		defer cancel()
-
-		log.Println("shutting down server...")
-		return server.Shutdown(timeoutCtx)
-	})
-
 	// 捕获到 os 退出信号将会退出
-	g.Go(func() error {
-		quit := make(chan os.Signal, 0)
+	eg.Go(func() error {
+		quit 		:= make(chan os.Signal, 0)
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
 		select {
 		case <-ctx.Done():
 			return ctx.Err()
 		case sig := <-quit:
-			return errors.Errorf("get os signal: %v", sig)
+			return errors.Errorf("quit_ch: get os signal: %v", sig)
 		}
 	})
 
-	log.Printf("errgroup exiting: %+v\n", g.Wait())
+	// http 控制
+	eg.Go(func() error {
+		select {
+		case <-ctx.Done():
+			log.Println("ch: errgroup Done...")
+		case <-serverOut:
+			log.Println("ch: server(shutdown) out...")
+		}
+
+		timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		log.Println("Server shutdown")
+		return server.Shutdown(timeoutCtx)
+	})
+
+	log.Printf("errgroup exit: %+v\n", eg.Wait())
 }
 
